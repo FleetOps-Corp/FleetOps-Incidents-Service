d8ee050a58ef8cc1a2d098e59228324b81350004
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2026-06-25 12:50:43 -0500

    format code with black

--- tests/integration/test_register_incident_e2e.py
+++ tests/integration/test_register_incident_e2e.py
@@ -3,7 +3,8 @@
 import pytest
 from datetime import datetime
 from unittest.mock import Mock
-#from uuid import uuid4
+
+# from uuid import uuid4
 
 # from incidents.domain.models import Incident
 from incidents.domain.services import IncidentService, VehicleValidatorService
